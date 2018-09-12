8bea9fa540adc8c9c69f78618052a7828100ba0c
Hyun woo son lee <hson@advlatam.com>
2018-09-12 10:55:56 -0500

    Added thymeleaf hello world

---
+++ thymeleaf/src/main/java/org/hson/helloWorld/config/HelloWorldApplication.java
@@ -0,0 +1,14 @@
+package org.hson.helloWorld.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication(scanBasePackages = "org.hson")
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+}
---
+++ thymeleaf/src/main/java/org/hson/helloWorld/controller/ViewController.java
@@ -0,0 +1,14 @@
+package org.hson.helloWorld.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+
+    @GetMapping("/view")
+    public String home(){
+        return "home";
+    }
+
+}
