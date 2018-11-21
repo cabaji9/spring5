46c31d0c7a60cb18bca49ae1b483a3bca52c106a
Hyun woo son lee <hson@advlatam.com>
2018-11-21 08:55:09 -0500

    Added spring security webflux with repository

---
+++ webflux/src/test/java/org/hson/webflux/WebfluxApplicationTests.java
@@ -0,0 +1,18 @@
+package org.hson.webflux;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {WebfluxApplicationTests.class})
+@SpringBootTest
+public class WebfluxApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
